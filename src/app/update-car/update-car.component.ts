94f734714371af909ee6beeb463f6c4ba0f39a9b
Laykaftw <azerbensassi@gmail.com>
2023-10-29 16:08:49 +0100

    Updating addcar and modifyingcar

--- src/app/update-car/update-car.component.ts
+++ src/app/update-car/update-car.component.ts
@@ -3,26 +3,33 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { CarService } from '../services/car.service';
 import { Car } from '../model/car.model';
-
+import { Categorie } from '../model/categorie.model';
 
 @Component({
   selector: 'app-update-car',
   templateUrl: './update-car.component.html',
-  styles: []
+  styles: [],
 })
 export class UpdateCarComponent {
   currentCar = new Car();
-  constructor(private activatedRoute: ActivatedRoute,
-              private router : Router,
-              private carService: CarService) { }
+  categories!: Categorie[];
+  updatedCatId!: number;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private carService: CarService
+  ) { }
   ngOnInit() {
     // console.log(this.activatedRoute.snapshot.params['id']);
-    this.currentCar = this.carService.consulterCar(this.activatedRoute.snapshot. params['id']);
-    console.log(this.currentCar);
-    }
-    updateCar(){
-      // console.log(this.currentCar);
-      this.carService.updateCar(this.currentCar),
-      this.router.navigate(['cars'])
-    }
+    this.categories = this.carService.listeCategories();
+    this.currentCar = this.carService.consulterCar(this.activatedRoute.snapshot.params['id']);
+    this.updatedCatId = this.currentCar.categorie.idCat;
+    // console.log(this.currentCar);
+  }
+  updateCar() {
+    // console.log(this.currentCar);
+    this.currentCar.categorie=this.carService.consulterCategorie(this.updatedCatId)
+    this.carService.updateCar(this.currentCar), 
+    this.router.navigate(['cars']);
+  }
 }
